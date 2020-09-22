3dd9b7f0ce19beccef114dea8b426e830fe0d397
Jonathan Yan <hanruy@andrew.cmu.edu>
2020-09-22 05:32:33 -0400

    game 2

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -10,25 +10,30 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
-#include <random>
+#define _USE_MATH_DEFINES
 
-GLuint hexapod_meshes_for_lit_color_texture_program = 0;
-Load< MeshBuffer > hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const * {
-	MeshBuffer const *ret = new MeshBuffer(data_path("hexapod.pnct"));
-	hexapod_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
+#include <cmath>
+#include <random>
+#include <sstream>
+#include <string>
+
+GLuint food_meshes_for_lit_color_texture_program = 0;
+Load< MeshBuffer > food_meshes(LoadTagDefault, []() -> MeshBuffer const * {
+	MeshBuffer const *ret = new MeshBuffer(data_path("brunch_food.pnct"));
+	food_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
 	return ret;
 });
 
-Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
-	return new Scene(data_path("hexapod.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
-		Mesh const &mesh = hexapod_meshes->lookup(mesh_name);
+Load< Scene > food_scene(LoadTagDefault, []() -> Scene const * {
+	return new Scene(data_path("brunch_food.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
+		Mesh const &mesh = food_meshes->lookup(mesh_name);
 
 		scene.drawables.emplace_back(transform);
 		Scene::Drawable &drawable = scene.drawables.back();
 
 		drawable.pipeline = lit_color_texture_program_pipeline;
 
-		drawable.pipeline.vao = hexapod_meshes_for_lit_color_texture_program;
+		drawable.pipeline.vao = food_meshes_for_lit_color_texture_program;
 		drawable.pipeline.type = mesh.type;
 		drawable.pipeline.start = mesh.start;
 		drawable.pipeline.count = mesh.count;
@@ -36,32 +41,69 @@ Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
 	});
 });
 
-PlayMode::PlayMode() : scene(*hexapod_scene) {
+PlayMode::PlayMode() : scene(*food_scene) {
 	//get pointers to leg for convenience:
 	for (auto &transform : scene.transforms) {
-		if (transform.name == "Hip.FL") hip = &transform;
-		else if (transform.name == "UpperLeg.FL") upper_leg = &transform;
-		else if (transform.name == "LowerLeg.FL") lower_leg = &transform;
-	}
-	if (hip == nullptr) throw std::runtime_error("Hip not found.");
-	if (upper_leg == nullptr) throw std::runtime_error("Upper leg not found.");
-	if (lower_leg == nullptr) throw std::runtime_error("Lower leg not found.");
+    std::cout << transform.name << std::endl;
+    if (transform.name == "cakePlate") {
+      cake.self = &transform;
+      targets.push_back(&cake);
+    } else if (transform.name == "eggBaconPlate") {
+      eggb.self = &transform;
+      targets.push_back(&eggb);
+    } else if (transform.name == "eggBaconPlate2") {
+      eggb2.self = &transform;
+      targets.push_back(&eggb2);
+    } else if (transform.name == "pancakePlate") {
+      pan.self = &transform;
+      targets.push_back(&pan);
+    } else if (transform.name == "sandwichPlate") {
+      sand.self = &transform;
+      targets.push_back(&sand);
+    }
+  }
 
-	hip_base_rotation = hip->rotation;
-	upper_leg_base_rotation = upper_leg->rotation;
-	lower_leg_base_rotation = lower_leg->rotation;
+	if (cake.self == nullptr) throw std::runtime_error("Cake not found.");
+	if (eggb.self == nullptr) throw std::runtime_error("Egg bacon 1 not found.");
+	if (eggb2.self == nullptr) throw std::runtime_error("Egg bacon 2 not found.");
+	if (pan.self == nullptr) throw std::runtime_error("Pancake not found.");
+	if (sand.self == nullptr) throw std::runtime_error("Sandwich not found.");
 
 	//get pointer to camera for convenience:
 	if (scene.cameras.size() != 1) throw std::runtime_error("Expecting scene to have exactly one camera, but it has " + std::to_string(scene.cameras.size()));
 	camera = &scene.cameras.front();
+
+  setup();
 }
 
 PlayMode::~PlayMode() {
 }
 
+void PlayMode::setup() {
+  shots_left = shots_max;
+  score = 0;
+  time = time_max;
+
+  for(auto& t : targets) {
+    reset_target(t);
+  }
+
+  running = true;
+
+  camera->transform->position.x = 30;
+  camera->transform->position.y = 36;
+  camera->transform->position.z = 305;
+}
+
 bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size) {
 
 	if (evt.type == SDL_KEYDOWN) {
+    if(!running) {
+      // Restart the game
+      setup();
+      return true;
+    }
+
 		if (evt.key.keysym.sym == SDLK_ESCAPE) {
 			SDL_SetRelativeMouseMode(SDL_FALSE);
 			return true;
@@ -81,7 +123,25 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			down.downs += 1;
 			down.pressed = true;
 			return true;
-		}
+		} else if (evt.key.keysym.sym == SDLK_SPACE) {
+      // Shoot targets
+      shots_left--;
+
+      for(auto& t : targets) {
+        float dist = glm::length(glm::cross(t->self->position - glm::vec3(0.0f, 0.0f, 0.0f), t->self->position - camera->transform->position)) / glm::length(camera->transform->position); 
+
+        if(dist < target_size) {
+          // Hit
+          score++;
+          reset_target(t);
+        }
+      }
+      return true;
+    } else if (evt.key.keysym.sym == SDLK_r) {
+      // Quick restart
+      setup();
+      return true;
+    }
 	} else if (evt.type == SDL_KEYUP) {
 		if (evt.key.keysym.sym == SDLK_a) {
 			left.pressed = false;
@@ -119,24 +179,61 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 	return false;
 }
 
+void PlayMode::reset_target(Target* target) {
+  static std::mt19937 mt;
+
+  target->self_rot_speed = (mt() / float(mt.max())) * 1 + 1;
+
+  float rot_axis_x = (mt() / float(mt.max())) * 2 - 1;
+  float rot_axis_angle = (mt() / float(mt.max())) * 2 * float(M_PI);
+  target->self_rot_axis.x = rot_axis_x;
+  target->self_rot_axis.y = (1 - rot_axis_x * rot_axis_x) * std::sin(rot_axis_angle);
+  target->self_rot_axis.z = (1 - rot_axis_x * rot_axis_x) * std::cos(rot_axis_angle);
+
+  target->cur_rot = 0;
+
+  target->radius = (mt() / float(mt.max())) * 50 + 50;
+  target->pos_speed = (mt() / float(mt.max())) * 0.25 + 0.25;
+
+  float pos_axis_x = (mt() / float(mt.max())) * 2 - 1;
+  float pos_axis_angle = (mt() / float(mt.max())) * 2 * float(M_PI);
+  target->pos_axis.x = pos_axis_x;
+  target->pos_axis.y = (1 - pos_axis_x * pos_axis_x) * std::sin(pos_axis_angle);
+  target->pos_axis.z = (1 - pos_axis_x * pos_axis_x) * std::cos(pos_axis_angle);
+
+  target->initial_pos = target->radius * glm::normalize(
+      glm::cross(target->pos_axis, target->self_rot_axis));
+
+  target->cur_pos = 0;
+}
+
 void PlayMode::update(float elapsed) {
+  if(!running) return;
 
-	//slowly rotates through [0,1):
-	wobble += elapsed / 10.0f;
-	wobble -= std::floor(wobble);
+  time -= elapsed;
+  if(shots_left <= 0 || time < 0) {
+    running = false;
+  }
 
-	hip->rotation = hip_base_rotation * glm::angleAxis(
-		glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 1.0f, 0.0f)
-	);
-	upper_leg->rotation = upper_leg_base_rotation * glm::angleAxis(
-		glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
-	lower_leg->rotation = lower_leg_base_rotation * glm::angleAxis(
-		glm::radians(10.0f * std::sin(wobble * 3.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
+  for(auto& t : targets) {
+    t->cur_rot += elapsed;
+    t->cur_pos += elapsed;
+
+    // Update self-rotation
+    //t->self->rotation *= glm::angleAxis(glm::radians(t->self_rot * std::sin(t->cur_rot * float(M_PI))), t->self_rot_axis);
+    t->self->rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f) * glm::angleAxis(
+    t->cur_rot * t->self_rot_speed * float(M_PI),
+		t->self_rot_axis);
+
+    // Update position
+    // Calculate rotation with Rodrigues' rotation formula
+    float angle = t->cur_pos * t->pos_speed * float(M_PI);
+    glm::vec3 axis = t->pos_axis;
+    glm::vec3 initial = t->initial_pos;
+    t->self->position = std::cos(angle) * initial +
+      std::sin(angle) * glm::cross(axis, initial) +
+      (1 - std::cos(angle)) * (axis * initial) * axis;
+  }
 
 	//move camera:
 	{
@@ -201,13 +298,15 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 			0.0f, 0.0f, 0.0f, 1.0f
 		));
 
+    std::ostringstream text;
+    text << "Score " << score << "  Shots left " << shots_left << "  Time left " << time;
 		constexpr float H = 0.09f;
-		lines.draw_text("Mouse motion rotates camera; WASD moves; escape ungrabs mouse",
+		lines.draw_text(text.str(),
 			glm::vec3(-aspect + 0.1f * H, -1.0 + 0.1f * H, 0.0),
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0x00, 0x00, 0x00, 0x00));
 		float ofs = 2.0f / drawable_size.y;
-		lines.draw_text("Mouse motion rotates camera; WASD moves; escape ungrabs mouse",
+		lines.draw_text(text.str(),
 			glm::vec3(-aspect + 0.1f * H + ofs, -1.0 + + 0.1f * H + ofs, 0.0),
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0xff, 0xff, 0xff, 0x00));
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -11,10 +11,32 @@ struct PlayMode : Mode {
 	PlayMode();
 	virtual ~PlayMode();
 
+  struct Target {
+    Scene::Transform* self = nullptr;
+
+    float radius;
+    float pos_speed;
+    glm::vec3 pos_axis;
+    glm::vec3 initial_pos;
+    float cur_pos;
+
+    float self_rot_speed;
+    glm::vec3 self_rot_axis;
+    float cur_rot;
+  } cake, eggb, eggb2, pan, sand;
+
 	//functions called by main loop:
 	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
 	virtual void update(float elapsed) override;
 	virtual void draw(glm::uvec2 const &drawable_size) override;
+
+  virtual void setup();
+  virtual void reset_target(Target* target);
+
+  // Game constants
+  static constexpr int shots_max = 10;
+  static constexpr float time_max = 10.0f;
+  static constexpr float target_size = 30.0f;
 
 	//----- game state -----
 
@@ -24,19 +46,19 @@ struct PlayMode : Mode {
 		uint8_t pressed = 0;
 	} left, right, down, up;
 
+  // Game stats
+  int shots_left;
+  int score;
+  float time;
+
 	//local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
 
-	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
-	
+  std::vector<Target*> targets;
+
 	//camera:
 	Scene::Camera *camera = nullptr;
+
+  bool running;
 
 };
